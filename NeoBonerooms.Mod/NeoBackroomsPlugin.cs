1e203695583a4fca48a2a74bc415221f7c96121e
sofiaslost <sofiaslost@COMET>
2025-10-25 22:17:11 +0100

    Add project files.

---
+++ NeoBonerooms.Mod/NeoBackroomsPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace NeoBonerooms.Mod
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class NeoBackroomsPlugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "org.neowayy.neobonerooms";
+        public const string PLUGIN_NAME = "NeoBonerooms";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ NeoBonerooms.Mod/UI/DisplayUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NeoBonerooms.Mod.UI
+{
+    public class DisplayUI : MonoBehaviour
+    {
+        public List<string> lines = new();
+    }
+}
